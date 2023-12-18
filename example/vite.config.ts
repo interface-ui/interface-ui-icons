c840b08ad327bbded3d78b7966810b648bf96fa5
ShihaoXiong <shihao.xiong@icloud.com>
2023-12-18 18:41:59 -0500

    chore: auto deploy configuration

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -15,5 +15,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/interface-ui-icons/',
   // plugins: [vue(), Components({ resolvers: [InterfaceIconResolver()] })],
 })
