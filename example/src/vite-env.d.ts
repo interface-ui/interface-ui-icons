e95de158f3f0a39dac0ba5c3977802c1a5270b42
ShihaoXiong <shihao.xiong@icloud.com>
2023-12-18 00:01:06 -0500

    chore: configuration for declaration

--- example/src/vite-env.d.ts
+++ example/src/vite-env.d.ts
@@ -1,5 +1,1 @@
 /// <reference types="vite/client" />
-declare module '@interface-ui/icons/es/components/*' {
-  import * as icons from '@interface-ui/icons/es/components/index'
-  export default icons
-}
--- packages/material-icons/vite.config.ts
+++ packages/material-icons/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts({ outDir: 'es' }), dts({ outDir: 'lib' })],
   build: {
     target: 'modules',
     minify: true,
