8d009cc91a85ae9a9ada6a47b29e0774d283b68d
tsinghua-lau <408285649@qq.com>
2023-12-20 19:27:59 +0800

    feat: update ESLint configuration

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  stylistic: {
+    indent: 2, // 4, or 'tab'
+    quotes: 'single', // or 'double'
+  },
+
+  // TypeScript and Vue are auto-detected, you can also explicitly enable them:
+  typescript: true,
+  vue: true,
+
+  // Disable jsonc and yaml support
+  jsonc: false,
+  yaml: false,
+
+  // `.eslintignore` is no longer supported in Flat config, use `ignores` instead
+  ignores: [
+    './packages/material-icons/src/components/**',
+  ],
+})
